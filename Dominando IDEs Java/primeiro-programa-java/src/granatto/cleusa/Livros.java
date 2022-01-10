277758a0671c05e1d9d96123a9cee010a7aba1ee
Cleusa Granatto <cleusafgranatto@gmail.com>
2022-01-10 17:07:55 -0300

    commit do curso dominando IDEs Java

---
+++ Dominando IDEs Java/primeiro-programa-java/src/granatto/cleusa/Livros.java
@@ -0,0 +1,5 @@
+package granatto.cleusa;
+
+public class Livros {
+
+}
---
+++ Dominando IDEs Java/primeiro-programa-java/src/granatto/cleusa/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package granatto.cleusa;
+
+import granatto.cleusa.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		
+	}
+}
+
+class Livro {
+	private String nome;
+	private String npag;
+}
+
